c0b54e1dad4ce36ca061a8bfb2db5bf72688238e
Matthew Semeniuk <megaman22342@live.ca>
2021-01-31 17:36:35 +0000

    Added variable shield support

--- pages/api/image/generate.js
+++ pages/api/image/generate.js
@@ -19,12 +19,16 @@ const options = { x: 0, y: 0, fontSize: 36, anchor: 'top', attributes: attribute
 export default async function handler(req, res) {
 	const p1 = req.body.p1;
 	const p2 = req.body.p2;
+	const p1Shields = req.body.p1Shields;
+	const p2Shields = req.body.p2Shields;
 		
 	const p1Name = textToSVG.getSVG(p1.name, options);
 	const p1Metrics = textToSVG.getMetrics(p1.name, options);
-	
+	const p1Cp = textToSVG.getSVG('CP: ' + p1.cp, options);
+
 	const p2Name = textToSVG.getSVG(p2.name, options);
     const p2Metrics = textToSVG.getMetrics(p2.name, options);
+	const p2Cp = textToSVG.getSVG('CP: ' + p2.cp, options);
 	
 	const p1HealthPercentage = Math.floor(p1.hp/p1.maxHP)*100;
 	
@@ -32,7 +36,7 @@ export default async function handler(req, res) {
     const shield = await sharp('public/battle/shield.png')
         .resize({ width: 35, height: p1Metrics.height })
         .toBuffer();
-		    
+
     const leftPokemon = await sharp(p1.path)
         .flop()
         .toBuffer();
@@ -48,19 +52,39 @@ export default async function handler(req, res) {
         { input: rightPokemon, top: 300, left: leftMargin },
         { input: Buffer.from(p1Name), top: 0, left: 0 },
         { input: Buffer.from(p2Name), top: 0, left: leftMargin },
-        { input: shield, top: 0, left: p1Metrics.width+10 },
-        { input: shield, top: 0, left: p1Metrics.width+55 },
-        { input: shield, top: 0, left: leftMargin-35-10 },
-        { input: shield, top: 0, left: leftMargin-35-55 },
-        { input: Buffer.from(getHealth(p1.hp, p1.maxHP)), top: p2Metrics.height+10, left: 5 },
-        { input: Buffer.from(getHealth(p2.hp, p2.maxHP)), top: p2Metrics.height+10, left: leftMargin }])
-        .toFile(`public/battle/${req.body.userId}.png`);
+		{ input: Buffer.from(p1Cp), top: p2Metrics.height+10, left: 0 },
+		{ input: Buffer.from(p2Cp), top: p2Metrics.height+10, left: leftMargin }])
+		
+	if(p1Shields == 2) {
+		await background.composite([
+		    { input: shield, top: 0, left: p1Metrics.width+10 },
+			{ input: shield, top: 0, left: p1Metrics.width+55 },
+		]);
+	}
+	else if(p1Shields == 1) {
+		await background.composite([
+		    { input: shield, top: 0, left: p1Metrics.width+10 },
+		]);
+	}
+	if(p2Shields == 2) {
+		await background.composite([
+		    { input: shield, top: 0, left: leftMargin-35-10 },
+			{ input: shield, top: 0, left: leftMargin-35-55 },
+		]);
+	}
+	else if(p2Shields == 1) {
+		await background.composite([
+		    { input: shield, top: 0, left: leftMargin-35-10 },
+		]);
+	}
+	
+	await background.toFile(`public/battle/${req.body.userId}.png`);
 		
 	res.end();
 }
 
 function getHealth(currentHP, maxHP) {
-	const remaining = Math.floor(currentHP/maxHP)*100;
+	const remaining = Math.floor(currentHP/maxHP*100);
 	return `
 		<svg xmlns="http://www.w3.org/2000/svg">
 			<defs>
