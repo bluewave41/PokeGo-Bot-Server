a873092b4c8840fbb30caf9b2be6216b9dc01031
Matthew Semeniuk <megaman22342@live.ca>
2021-02-24 18:03:18 +0000

    Fixed import

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const { createServer } = require('https');
 const { parse } = require('url');
 const next = require('next');
+const fs = require('fs');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev })
